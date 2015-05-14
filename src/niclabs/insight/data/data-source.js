9292bd04e26f7ae492d229d52f183152641a9940
Felipe Lalanne <flalanne@niclabs.cl>
2015-05-14 15:00:21 -0300

    Created the DataSource interface for generic data sources

---
+++ src/niclabs/insight/data/data-source.js
@@ -0,0 +1,41 @@
+niclabs.insight.data.DataSource = (function() {
+    /**
+     * Constructs a new data source
+     *
+     * A DataSource object encapsulates interaction with sources of data, whether they
+     * come from code defined arrays, a JSON/CSV remote source, API call, or other data sources
+     *
+     * @class niclabs.insight.data.DataSource
+     * @extends {niclabs.insight.Element}
+     * @param {Object} options - configuration options for the data source
+     * @param {String} options.id - identifier for referencing this data source
+     */
+    var constructor = function(options) {
+        var self = niclabs.insight.Element(options);
+
+        // TODO: we should find a way to notify that new data has been received
+
+        /**
+         * Iterate over the data source elements
+         *
+         * This method will iterate over the elements of the data source, passing their relative position in the
+         * data source and the value for the element in that position to the callback function.
+         *
+         * Although this method could sometimes be run synchronously (if the data source is an array), its better to assume
+         * that it runs asynchronously, specially when dealing with remote data sources
+         *
+         * The order of the iteration must be defined by each data source.
+         *
+         * @memberof niclabs.insight.data.DataSource
+         * @param {niclabs.insight.data.DataSource~useDataElement} fn - handler for the data element
+         * @abstract
+         */
+        self.each = function(fn) {
+            throw Error("Not implemented");
+        };
+
+        return self;
+    };
+
+    return constructor;
+})();
