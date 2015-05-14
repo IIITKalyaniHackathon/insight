df4f1f6f729640aa7cd131a1ab9f62484cfa3f80
Felipe Lalanne <flalanne@niclabs.cl>
2015-05-14 15:00:37 -0300

    Removed whitespace

--- src/niclabs/insight/_.js
+++ src/niclabs/insight/_.js
@@ -14,9 +14,7 @@ niclabs.insight = (function ($) {
     // Production steps of ECMA-262, Edition 5, 15.4.4.18
     // Reference: http://es5.github.io/#x15.4.4.18
     if (!Array.prototype.forEach) {
-
         Array.prototype.forEach = function (callback, thisArg) {
-
             var T, k;
 
             if (this === null) {
